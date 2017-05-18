2382647fcbb270a04700256730f93a311f769110
Nick Rupp <nickrupp95@gmail.com>
2017-05-18 20:49:08 +0100

    First Passing Test

---
+++ cross-nought/Game.swift
@@ -0,0 +1,15 @@
+//
+//  game.swift
+//  cross-nought
+//
+//  Created by Nick Rupp on 18/05/2017.
+//  Copyright © 2017 whatsrupp. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    var board:[[String]]=[["","",""],["","",""],["","",""]]
+    
+}
--- cross-noughtTests/GameTests.swift
+++ cross-noughtTests/GameTests.swift
@@ -1,17 +1,22 @@
 //
-//  cross_noughtTests.swift
-//  cross-noughtTests
+//  GameTests.swift
+//  cross-nought
 //
 //  Created by Nick Rupp on 18/05/2017.
 //  Copyright © 2017 whatsrupp. All rights reserved.
 //
 
 import XCTest
+
 @testable import cross_nought
 
-class cross_noughtTests: XCTestCase {
+class GameTests: XCTestCase {
+    
     let game = Game()
-        override func setUp() {
+    
+    let blankBoard: [[String]] = [["","",""],["","",""],["","",""]]
+    
+    override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -22,15 +27,15 @@ class cross_noughtTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+       
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func initializesWithEmptyBoard(){
+        XCTAssert(blankBoard[0]==game.board[0])
+        XCTAssert(blankBoard[1]==game.board[1])
+        XCTAssert(blankBoard[2]==game.board[2])
     }
+    
+    
     
 }
